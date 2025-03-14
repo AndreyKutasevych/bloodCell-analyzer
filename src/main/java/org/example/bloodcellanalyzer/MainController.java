206887045aa3e06f21f4dfaa365356f1eb24c62b
AndreyKutasevych <andrdroniuc@gmail.com>
2025-03-14 14:23:08 +0000

    implement image upload

--- src/main/java/org/example/bloodcellanalyzer/MainController.java
+++ src/main/java/org/example/bloodcellanalyzer/MainController.java
@@ -1,14 +1,51 @@
 package org.example.bloodcellanalyzer;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.stage.FileChooser;
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+
+import java.io.File;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button fileChooserButton;
+    @FXML
+    private ImageView imageView;
+    private PixelWriter pixelWriter;
+    private PixelReader pixelReader;
+    private Image image;
+    private WritableImage writableImage;
+
+    @FXML
+    private void fileChoosing() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
+        );
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            image = new Image(file.toURI().toString(),imageView.getFitWidth(),imageView.getFitHeight(),false,true);
+            pixelReader = image.getPixelReader();
+            writableImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());
+            pixelWriter = writableImage.getPixelWriter();
+            imageView.setImage(writableImage);
+            displayImage();
+        }
+    }
+    @FXML
+    private void displayImage() {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixelWriter.setColor(x, y, pixelReader.getColor(x, y));
+            }
+        }
+
     }
 }
