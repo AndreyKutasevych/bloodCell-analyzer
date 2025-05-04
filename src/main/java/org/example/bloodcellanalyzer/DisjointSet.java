05cd0878566f0be96f6aa928c688d35369ce3a31
AndreyKutasevych <andrdroniuc@gmail.com>
2025-05-04 16:20:32 +0100

    comment the work

--- src/main/java/org/example/bloodcellanalyzer/DisjointSet.java
+++ src/main/java/org/example/bloodcellanalyzer/DisjointSet.java
@@ -3,28 +3,28 @@ package org.example.bloodcellanalyzer;
 public class DisjointSet {
 
     public static int find(int[] a, int id) {
-        if (a[id] < 0) {
+        if (a[id] < 0) {//find method with path compression
             return id;
         } else {
-            return a[id] = find(a, a[id]);
+            return a[id] = find(a, a[id]);//reference to the parent of parent
         }
     }
 
     public static void union(int[] a, int p, int q) {
-        int rootp = find(a, p);
-        int rootq = find(a, q);
+        int rootP = find(a, p);
+        int rootQ = find(a, q);
 
-        if (rootp == rootq) {
+        if (rootP == rootQ) {
             return; // Already connected
         }
 
         // Union by size: attach smaller tree to larger tree
-        if (a[rootp] < a[rootq]) {
-            a[rootp] += a[rootq];
-            a[rootq] = rootp;
+        if (a[rootP] < a[rootQ]) {//pixel value determines the set it belongs to, and its size - so if one set is smaller than another - connect it to the second one
+            a[rootP] += a[rootQ];
+            a[rootQ] = rootP;
         } else {
-            a[rootq] += a[rootp];
-            a[rootp] = rootq;
+            a[rootQ] += a[rootP];
+            a[rootP] = rootQ;
         }
     }
 }
--- src/main/java/org/example/bloodcellanalyzer/MainController.java
+++ src/main/java/org/example/bloodcellanalyzer/MainController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -12,11 +11,10 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private Slider cellSizeSlider;
@@ -36,7 +34,6 @@ public class MainController {
     private PixelReader pixelReader;
     private PixelWriter pixelWriterRGB;
     private Image image;
-    private WritableImage writableImage;
     private WritableImage writableImageRGB;
     private int[] pixelArray;
     private double redFilterValue;
@@ -52,22 +49,23 @@ public class MainController {
         cellSizeSlider.setMin(20);
         cellSizeSlider.setMax(500);
 
-        redFilter.valueProperty().addListener((observable, oldValue, newValue) -> {
+        //adding listeners to the sliders
+        redFilter.valueProperty().addListener((_, _, _) -> {
             redFilterValue=redFilter.getValue();
             displayBothImages();
         });
-        blueFilter.valueProperty().addListener((observable, oldValue, newValue) -> {
+        blueFilter.valueProperty().addListener((_, _, _) -> {
             blueFilterValue=blueFilter.getValue();
             displayBothImages();
         });
-        cellSizeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+        cellSizeSlider.valueProperty().addListener((_, _, _) -> {
             minimalCellSize=(int)cellSizeSlider.getValue();
             displayBothImages();
         });
     }
 
     @FXML
-    private void fileChoosing() {
+    private void fileChoosing() { // file selection and
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open File");
         fileChooser.getExtensionFilters().addAll(
@@ -77,25 +75,25 @@ public class MainController {
         if (file != null) {
             image = new Image(file.toURI().toString(),imageView.getFitWidth(),imageView.getFitHeight(),false,true);
             pixelReader = image.getPixelReader();
-            writableImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());
-            writableImageRGB = new WritableImage((int) image.getWidth(), (int) image.getHeight());
-            pixelWriter = writableImage.getPixelWriter();
+            WritableImage writableImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());// setting the actual image
+            writableImageRGB = new WritableImage((int) image.getWidth(), (int) image.getHeight());//setting the tricolor converted image
+            pixelWriter = writableImage.getPixelWriter();//setting corresponding pixel writers
             pixelWriterRGB = writableImageRGB.getPixelWriter();
             imageView.setImage(writableImage);
             imageViewRGB.setImage(writableImageRGB);
-            displayBothImages();
+            displayBothImages();//straight away calling a method to display images
         }
     }
 
     @FXML
     private void displayBothImages() {
         rectangleClear();
-        for (int x = 0; x < image.getWidth(); x++) {
+        for (int x = 0; x < image.getWidth(); x++) {//looping by width and then height pixel by pixel
             for (int y = 0; y < image.getHeight(); y++) {
-                pixelWriter.setColor(x, y, pixelReader.getColor(x, y));
+                pixelWriter.setColor(x, y, pixelReader.getColor(x, y));//setting actual pixel colors to the normal image
                 double red = pixelReader.getColor(x,y).getRed();
-                double blue = pixelReader.getColor(x,y).getBlue();
-                if(red<=redFilterValue  && red>blue){
+                double blue = pixelReader.getColor(x,y).getBlue();//getting red and blue channels for the blood cells
+                if(red<=redFilterValue  && red>blue){//comparing thresholds to the red or blue channels of image(you can tweak thresholds by using sliders
                     pixelWriterRGB.setColor(x, y, Color.RED);
                 } else if (blue<=blueFilterValue  && blue>red) {
                     pixelWriterRGB.setColor(x, y, Color.PURPLE);
@@ -105,58 +103,73 @@ public class MainController {
                 }
             }
         }
-        pixelArrayCreation();
+        pixelArrayCreation();//calling method to assign all the pixels to an array(initial values are -1 - those are going to be used for making disjoint sets from a list)
         union();
     }
 
     public void pixelArrayCreation(){
         pixelArray=new int[(int)writableImageRGB.getWidth()*(int)writableImageRGB.getHeight()];
-        for (int i = 0; i < pixelArray.length; i++) {
-            pixelArray[i]=-1;
-        }
+        Arrays.fill(pixelArray, -1);//simply iterating through every pixel of an image and assigning it to an array
     }
 
     @FXML
     public void union() {
         pixelArrayCreation();
-        int position = 0;
+
+        int position = 0; // temporary index of pixel(for union-find)
+
+        // Looping through every pixel in the image, row by row
         for (int y = 0; y < image.getHeight(); y++) {
             for (int x = 0; x < image.getWidth(); x++) {
                 Color current = writableImageRGB.getPixelReader().getColor(x, y);
+
+                //  Process only blood cells -red or blue - (red or purple pixels)
                 if (current.equals(Color.RED) || current.equals(Color.PURPLE)) {
+
+                    // Check right neighbor (x+1) if it's within bounds
                     if (x + 1 < image.getWidth()) {
                         Color right = writableImageRGB.getPixelReader().getColor(x + 1, y);
                         if (current.equals(right)) {
+                            // Union current and right pixel if they are the same
                             DisjointSet.union(pixelArray, position, position + 1);
                         }
                     }
+
+                    // Checking bottom neighbor if it's within bounds
                     if (y + 1 < image.getHeight()) {
                         Color down = writableImageRGB.getPixelReader().getColor(x, y + 1);
                         if (current.equals(down)) {
+                            // union current and bottom pixel if they are the same
                             DisjointSet.union(pixelArray, position, position + (int) image.getWidth());
                         }
                     }
+
                 } else {
+                    // If the pixel is background (not a cell), it is marked as -2
+                    // This ensures it's excluded from cell grouping logic
                     pixelArray[position] = -2;
                 }
-                position++;
+
+                position++; // Move to the next pixel's index
             }
         }
+
         rectangleDraw();
     }
+
 
     @FXML
     private void rectangleDraw() {
         rectangleClear();
-        List<Integer> rootList = new ArrayList<>();
+        List<Integer> rootList = new ArrayList<>();// making a list of roots, as each cell has its own root
 
         for (int i = 0; i < pixelArray.length; i++) {
-            if (pixelArray[i] < -minimalCellSize) {
+            if (pixelArray[i] < -minimalCellSize) {//comparing each pixel to the minimum cell size threshold, as  each root is a negative number with abs being the size of a cell
                 rootList.add(i);
             }
         }
 
-        int cellIndex = 1;
+        int cellIndex = 1;//cell counter for counting all cells on the image
         for (int root : rootList) {
             int[] outerBounds = getOuterBoundOfCell(root);
 
@@ -165,14 +178,14 @@ public class MainController {
             int minY = outerBounds[2];
             int maxY = outerBounds[3];
 
-            // Determine color type from a representative pixel (e.g., the root pixel)
+            // Determining color type from a root pixel
             int x = root % (int) writableImageRGB.getWidth();
             int y = root / (int) writableImageRGB.getWidth();
             Color rootColor = writableImageRGB.getPixelReader().getColor(x, y);
 
             Color cellColor = rootColor.equals(Color.RED) ? Color.GREEN : Color.BLUE;
 
-            Rectangle rect = new Rectangle(
+            Rectangle rect = new Rectangle(//making a rectangle of coordinates for each cell
                     minX,
                     minY,
                     maxX - minX,
@@ -181,9 +194,9 @@ public class MainController {
             rect.setStroke(cellColor);
             rect.setFill(Color.TRANSPARENT);
 
-            Text label = new Text(String.valueOf(cellIndex));
+            Text label = new Text(String.valueOf(cellIndex));//also adding label with cell index to each rectangle
             label.setFill(cellColor);
-            label.setX(minX + 5);
+            label.setX(minX + 5);//setting the text within the rectangle
             label.setY(minY + 15);
 
             mainPane.getChildren().addAll(rect, label);
@@ -194,30 +207,30 @@ public class MainController {
     }
 
 
-    private void rectangleClear(){
+    private void rectangleClear(){//simple way of clearing everything once image is updated
         mainPane.getChildren().removeIf(node -> node instanceof Rectangle);
         mainPane.getChildren().removeIf(node -> node instanceof Text);
     }
 
-    private int[] getOuterBoundOfCell(int root){
-        int minX = (int) writableImageRGB.getWidth();
+    private int[] getOuterBoundOfCell(int root){//providing a method with root pixel
+        int minX = (int) writableImageRGB.getWidth();//initially this is just a one big rectangle
         int maxX = 0;
         int minY = (int) writableImageRGB.getHeight();
         int maxY = 0;
 
-        for (int i = 0; i < pixelArray.length; i++) {
+        for (int i = 0; i < pixelArray.length; i++) {//looping again
             if (DisjointSet.find(pixelArray, i) == root) {
-                int x = i % (int) writableImageRGB.getWidth();
+                int x = i % (int) writableImageRGB.getWidth();//root can also be on the bounds
                 int y = i / (int) writableImageRGB.getWidth();
 
-                if (x < minX) minX = x;
+                if (x < minX) minX = x;//setting the width and height of rectangle depending on its position in terms of root pixel
                 if (x > maxX) maxX = x;
                 if (y < minY) minY = y;
                 if (y > maxY) maxY = y;
             }
         }
 
-        return new int[] { minX+14, maxX+14, minY+64, maxY+64 };
+        return new int[] { minX+14, maxX+14, minY+64, maxY+64 };//adding difference from start of pane and start of image
     }
 
 }
